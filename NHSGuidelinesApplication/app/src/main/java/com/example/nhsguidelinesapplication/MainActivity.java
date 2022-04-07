a582e9edbea06dd8e74b26e7e848bb648624784a
Thomas-monke <x3h17@students.keele.ac.uk>
2022-04-07 15:50:39 +0100

    Added Android Studio Project

---
+++ NHSGuidelinesApplication/app/src/main/java/com/example/nhsguidelinesapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nhsguidelinesapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate() {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
