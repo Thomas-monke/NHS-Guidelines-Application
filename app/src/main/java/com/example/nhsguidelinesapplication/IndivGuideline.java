b403f0486046789e7ed6a6b2466e44e929cb50ce
Thomas-monke <x3h17@students.keele.ac.uk>
2022-04-09 18:40:18 +0100

    Implemented sidebar

--- app/src/main/java/com/example/nhsguidelinesapplication/IndivGuideline.java
+++ app/src/main/java/com/example/nhsguidelinesapplication/IndivGuideline.java
@@ -1,24 +1,24 @@
-package com.example.nhsguidelinesapplication;
-
-import android.database.Cursor;
-import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class IndivGuideline extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
-
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, new String[] {"NAME"}, new int[] {android.R.id.text1, android.R.id.text2}, 0);
-        listView.setAdapter(listAdapter);
-        AdapterView.OnItemClickListener itemClickListener = new MyListViewListener();
-        listView.setOnItemClickListener(itemClickListener);
-
-    }
-}
+//package com.example.nhsguidelinesapplication;
+//
+//import android.database.Cursor;
+//import android.os.Bundle;
+//import android.widget.AdapterView;
+//import android.widget.ListView;
+//import android.widget.SimpleCursorAdapter;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class IndivGuideline extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_list);
+//
+//        ListView listView = (ListView) findViewById(R.id.listView);
+//        listAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, new String[] {"NAME"}, new int[] {android.R.id.text1, android.R.id.text2}, 0);
+//        listView.setAdapter(listAdapter);
+//        AdapterView.OnItemClickListener itemClickListener = new MyListViewListener();
+//        listView.setOnItemClickListener(itemClickListener);
+//
+//    }
+//}
